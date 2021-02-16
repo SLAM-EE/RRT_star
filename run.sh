56ac1f46519df8e14acee23c187c40205e37d9d2
Aswin Ajayan <aswinp2008@gmail.com>
2021-02-16 10:07:52 +0530

    added a shell script to run ORB slam on captured sequence

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+./Examples/Monocular/mono_rt Vocabulary/ORBvoc.txt Examples/Monocular/WebCamLenovo.yaml  /home/custom_dataset/DS-01/ground_floor.webm
+
+#./Examples/Monocular/mono_rt Vocabulary/ORBvoc.txt Examples/Monocular/WebCamLenovo.yaml  /home/custom_dataset/DS-00/bumpy.webm
+
+#./Examples/Monocular/mono_rt Vocabulary/ORBvoc.txt Examples/Monocular/WebCamLenovo.yaml  /home/custom_dataset/DS-02/first_floor.webm
+
